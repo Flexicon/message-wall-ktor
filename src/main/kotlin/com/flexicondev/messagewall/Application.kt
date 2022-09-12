272919e9a79de1a03a0aea8cd14851d3f17b2113
Mike Repec <mikerepec@gmail.com>
2022-09-12 20:38:39 +0200

    Use application.conf

--- src/main/kotlin/com/flexicondev/messagewall/Application.kt
+++ src/main/kotlin/com/flexicondev/messagewall/Application.kt
@@ -4,14 +4,14 @@ import com.flexicondev.messagewall.plugins.configureHTTP
 import com.flexicondev.messagewall.plugins.configureMonitoring
 import com.flexicondev.messagewall.plugins.configureRouting
 import com.flexicondev.messagewall.plugins.configureSerialization
-import io.ktor.server.cio.CIO
-import io.ktor.server.engine.embeddedServer
+import io.ktor.server.application.Application
+import io.ktor.server.cio.EngineMain
 
-fun main() {
-    embeddedServer(CIO, port = 8080, host = "0.0.0.0") {
-        configureHTTP()
-        configureMonitoring()
-        configureSerialization()
-        configureRouting()
-    }.start(wait = true)
+fun main(args: Array<String>): Unit = EngineMain.main(args)
+
+fun Application.module() {
+    configureHTTP()
+    configureMonitoring()
+    configureSerialization()
+    configureRouting()
 }
