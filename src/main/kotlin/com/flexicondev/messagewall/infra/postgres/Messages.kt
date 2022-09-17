c639a7c3250b6fc4f0c1e4593536ef986edcafe6
Mike Repec <mikerepec@gmail.com>
2022-09-17 22:08:01 +0200

    Fix text column name

--- src/main/kotlin/com/flexicondev/messagewall/infra/postgres/Messages.kt
+++ src/main/kotlin/com/flexicondev/messagewall/infra/postgres/Messages.kt
@@ -4,7 +4,7 @@ import org.jetbrains.exposed.sql.Table
 
 object Messages : Table() {
     val id = integer("id").autoIncrement().primaryKey()
-    val text = text("name")
+    val text = text("text")
     val author = varchar("author", 255)
     val createdAt = datetime("created_at")
 }
