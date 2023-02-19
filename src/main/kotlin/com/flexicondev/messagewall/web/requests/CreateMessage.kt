9d2de7c39f8f2dc18d5a78b103db3b31d5f53e6b
Mike Repec <mikerepec@gmail.com>
2023-02-19 11:54:43 +0100

    Fix missing field validation

--- src/main/kotlin/com/flexicondev/messagewall/web/requests/CreateMessage.kt
+++ src/main/kotlin/com/flexicondev/messagewall/web/requests/CreateMessage.kt
@@ -4,6 +4,6 @@ import com.flexicondev.messagewall.domain.Message
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class CreateMessage(val text: String, val author: String) {
+data class CreateMessage(val text: String = "", val author: String = "") {
     fun toMessage() = Message(text = text, author = author)
 }
